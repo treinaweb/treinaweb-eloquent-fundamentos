59f96b907156a2526632c69aabd0fdf36c24af5f
Elton Fonseca <elton869@gmail.com>
2018-07-20 14:20:36 -0300

    11 - mapeando chave primaria e timestamp

--- app/Comment.php
+++ app/Comment.php
@@ -6,5 +6,39 @@ use Illuminate\Database\Eloquent\Model;
 
 class Comment extends Model
 {
-    //
+    /**
+     * Define nome da tabela do model
+     *
+     * @var string
+     */
+    protected $table = 'comentario';
+
+    /**
+     * Define coluna chave primária da tabela
+     *
+     * @var string
+     */
+    protected $primaryKey = 'codigo';
+
+    /**
+     * Define tipo da chave primária
+     *
+     * @var string
+     */
+    protected $keyType = 'string';
+
+    /**
+     * Define que a chave primária não é auto_increment
+     *
+     * @var boolean
+     */
+    public $incrementing = false;
+
+    /**
+     * Define que a tabela não terá colunas de timestamp
+     *
+     * @var boolean
+     */
+    public $timestamps = false;
+
 }
