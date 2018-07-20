8301dc175279f601e1744e0a4e07cc4898b7a9fc
Elton Fonseca <elton869@gmail.com>
2018-07-20 16:32:25 -0300

    14 - propriedade guarded

--- app/Post.php
+++ app/Post.php
@@ -6,5 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    protected $fillable = ['title', 'content'];
+    protected $guarded = ['id', 'created_at', 'updated_at'];
 }
