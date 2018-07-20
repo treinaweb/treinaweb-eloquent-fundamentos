1d6fc6e2cda33ba4d5561d20fec50033bc0476c4
Elton Fonseca <elton869@gmail.com>
2018-07-20 15:37:34 -0300

    12 - inserindo dados

--- app/Post.php
+++ app/Post.php
@@ -6,23 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    /**
-     * Define colunas do timestamp
-     */
-    const CREATED_AT = 'criacao';
-    const UPDATED_AT = 'atualizacao';
-
-    /**
-     * Define nome da tabela do model
-     *
-     * @var string
-     */
-    protected $table = 'artigo';
-
-    /**
-     * Define coluna chave primária da tabela
-     *
-     * @var string
-     */
-    protected $primaryKey = 'codigo';
+    //
 }
