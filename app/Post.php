a5719af508b88786143a9fd1c0ebacb6af88da6f
Elton Fonseca <elton869@gmail.com>
2018-07-25 19:02:18 -0300

    23 - Mapeando o relacionamento

--- app/Post.php
+++ app/Post.php
@@ -7,4 +7,15 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     protected $guarded = ['id', 'created_at', 'updated_at'];
+
+    /**
+     * Mapeia o relacionamento com o model details
+     *
+     * @return void
+     */
+    public function details()
+    {
+        return $this->hasOne('App\Details');
+    }
+
 }
