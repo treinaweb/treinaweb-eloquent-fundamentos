826496e7127200385d0759f76aa561cb6e87e0bb
Elton Fonseca <elton869@gmail.com>
2018-07-26 14:38:56 -0300

    25 - persistindo dados atraves da relaçao

--- app/Details.php
+++ app/Details.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Details extends Model
 {
+    protected $fillable = ['status', 'visibility'];
+
     /**
      * Mapeia o relacionamento com o model post
      *
